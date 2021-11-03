3e495169b244f5aded4b2b03cad2a412b2cfd77b
GelSight <debra_shure@gelsight.com>
2021-11-03 11:50:17 -0400

    add all .sos

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='gelsight',
     version='1.0',
     packages=['gelsight', 'gelsightcore'],
-    package_data={'gelsightcore': ['*.so']},
+    package_data={'gelsightcore': ['*.so*']},
     license='',
     author='gelsight',
     author_email='info@gelsight.com',
