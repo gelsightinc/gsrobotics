df6b2f1f1a3977d1c1d8b2f95dc7e7f1a560c71d
GelSight <debra_shure@gelsight.com>
2021-11-03 14:47:03 -0400

    #689 add device argument

--- connect.sh
+++ connect.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-ssh pi@gsr15demo <<EOF
+if [ "$#" -eq "0" ]
+then
+    REPODIR=$PWD
+else
+    RASPPI=$1
+fi
+
+ssh -t pi@$RASPPI <<EOF
     mjpg_streamer -i "input_raspicam.so -fps 60 -x 480 -y 640 -rot 270 -quality 100 -awb off -awbgainR 1 -awbgainB 1" -o "output_http.so -w ./www"
 EOF
 
