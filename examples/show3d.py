e634b74eb74fb27efbdc9fe99944015b33270258
GelSight <debra_shure@gelsight.com>
2021-11-03 15:48:29 -0400

    add device input arg

--- examples/show3d.py
+++ examples/show3d.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import sys, getopt
 import numpy as np
 import cv2
 import math
@@ -21,13 +22,27 @@ def get_diff_img(img1,img2):
 def get_diff_img_2(img1, img2):
     return (img1 * 1.0 - img2) / 255.  + 0.5
 
-if __name__ == "__main__":
+def main(argv):
 
+    device = ""
+    try:
+       opts, args = getopt.getopt(argv,"hd:",["device="])
+    except getopt.GetoptError:
+       print('show3d.py -d <device>')
+       sys.exit(2)
+    for opt, arg in opts:
+       if opt == '-h':
+          print('show3d.py -d <device>')
+          sys.exit()
+       elif opt in ("-d", "--device"):
+          device = arg
+
+    # Set flags 
     SAVE_VIDEO_FLAG = False
-    SAVE_PCD_FLAG = False
     GPU = False
     MASK_MARKERS_FLAG = True
-    #path = '/home/gelsight/Documents/test_images/R1.5/multiballcalib/trial5/'
+
+    # Path to 3d model
     path = '.'
 
     # Set the camera resolution
@@ -37,20 +52,20 @@ if __name__ == "__main__":
     # mmpp = 0.029 # for 1032x772 img size from R1
 
     finger = gsdevice.Finger.R15
+    capturestream = "http://" + device + ":8080/?action=stream"
 
     if finger == gsdevice.Finger.R1:
         dev = gsdevice.Camera(finger, 0)
         net_file_path = 'nnr1.pt'
     else:
         #cap = cv2.VideoCapture('http://gsr15demo.local:8080/?action=stream')
-        dev = gsdevice.Camera(finger, 'http://gsr15demo.local:8080/?action=stream')
+        dev = gsdevice.Camera(finger, capturestream)
         net_file_path = 'nnr15.pt'
 
     dev.connect()
 
     ''' Load neural network '''
     model_file_path = path
-    # net_file_path = "best_model_epoch_533_c3.1_f1_only7.95mm.pt"
     net_path = os.path.join(model_file_path, net_file_path)
     print('net path = ', net_path)
 
@@ -61,7 +76,7 @@ if __name__ == "__main__":
 
     if SAVE_VIDEO_FLAG:
         #### Below VideoWriter object will create a frame of above defined The output is stored in 'filename.avi' file.
-        file_path = '/home/radhen/Documents/Videos/3dnnlive.mov'
+        file_path = './3dnnlive.mov'
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
         out = cv2.VideoWriter(file_path, fourcc, 60, (160, 120), isColor=True)
 
@@ -88,3 +103,6 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
             print('Interrupted!')
             dev.stop_video()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
