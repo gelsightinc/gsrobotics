49c1fb0054b391766e04f612c4e6483f66a9ccc8
debrashure <debra_shure@gelsight.com>
2021-11-08 13:13:36 -0500

    change to 30 fps

--- connect.sh
+++ connect.sh
@@ -8,6 +8,6 @@ else
 fi
 
 ssh -t pi@$RASPPI <<EOF
-    mjpg_streamer -i "input_raspicam.so -fps 60 -x 480 -y 640 -rot 270 -quality 100 -awb off -awbgainR 1 -awbgainB 1" -o "output_http.so -w ./www"
+    mjpg_streamer -i "input_raspicam.so -fps 30 -x 480 -y 640 -rot 270 -quality 100 -awb off -awbgainR 1.75 -awbgainB 1.75" -o "output_http.so -w ./www"
 EOF
 
