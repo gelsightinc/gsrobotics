822ee4e51d345e9227ded6443af0b884f7ebc101
GelSight <debra_shure@gelsight.com>
2022-02-08 16:57:51 -0500

    fix usage

--- connect.sh
+++ connect.sh
@@ -2,12 +2,12 @@
 
 if [ "$#" -eq "0" ]
 then
-    REPODIR=$PWD
+    echo "USAGE $0 <raspberry pi name or address>" && exit
 else
     RASPPI=$1
 fi
 
-ssh -t pi@$RASPPI <<EOF
+ssh -T pi@$RASPPI <<EOF
     mjpg_streamer -i "input_raspicam.so -fps 30 -x 480 -y 640 -rot 270 -quality 100 -awb off -awbgainR 1.75 -awbgainB 1.75" -o "output_http.so -w ./www"
 EOF
 
