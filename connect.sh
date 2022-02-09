9db538fd36678b2212cca2a951e4921a6c10c67c
GelSight <debra_shure@gelsight.com>
2022-02-09 17:22:55 -0500

    stream at 240x320 60fps

--- connect.sh
+++ connect.sh
@@ -8,6 +8,6 @@ else
 fi
 
 ssh -T pi@$RASPPI <<EOF
-    mjpg_streamer -i "input_raspicam.so -fps 30 -x 480 -y 640 -rot 270 -quality 100 -awb off -awbgainR 1.75 -awbgainB 1.75" -o "output_http.so -w ./www"
+    mjpg_streamer -i "input_raspicam.so -fps 30 -x 480 -y 640 -rot 270 -quality 100 -awb off -awbgainR 1.0 -awbgainB 1.0" -o "output_http.so -w ./www"
 EOF
 
