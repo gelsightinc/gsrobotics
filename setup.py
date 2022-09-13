3e6676f1302d93eb68d756b550ce9d8b7b17279d
debrashure <debra_shure@gelsight.com>
2022-09-13 12:02:01 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gelsight',
-    version='0.1',
+    version='1.0',
     packages=['gelsight', 'gelsightcore'],
     package_data={'gelsightcore': ['*.so*']},
     license='',
