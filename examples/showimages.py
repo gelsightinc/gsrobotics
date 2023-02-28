797d6a6005d078768092f304e6b613343c0c117e
debrashure <debra_shure@gelsight.com>
2023-02-28 15:36:59 -0500

    cleanup code

--- examples/showimages.py
+++ examples/showimages.py
@@ -1,14 +1,6 @@
 import sys, getopt
-import numpy as np
 import cv2
-import math
-import os
-from os import listdir
-from os.path import isfile, join
-import open3d
-import copy
 from gelsight import gsdevice
-
 
 
 def main(argv):
@@ -33,13 +25,6 @@ def main(argv):
     MASK_MARKERS_FLAG = False
     FIND_ROI = False ## set True the first time to find ROI on an interactive window. Once values are found, use one of the else block.
 
-    # Set the camera resolution
-    # mmpp = 0.0887  # for 240x320 img size
-    # mmpp = 0.1778  # for 160x120 img size from R1
-    # mmpp = 0.0446  # for 640x480 img size R1
-    # mmpp = 0.029 # for 1032x772 img size from R1
-    mmpp = 0.075 # r2d2 gel 18x24mm at 240x320
-
     if device == "R1":
         finger = gsdevice.Finger.R1
     elif device[-5:] == "local":
@@ -58,7 +43,7 @@ def main(argv):
         #cap = cv2.VideoCapture('http://gsr15demo.local:8080/?action=stream')
         dev = gsdevice.Camera(finger, capturestream)
     elif finger == gsdevice.Finger.MINI:
-        # the device ID can change after chaning the usb ports.
+        # the device ID can change after unplugging and changing the usb ports.
         # on linux run, v4l2-ctl --list-devices, in the terminal to get the device ID for camera
         cam_id = gsdevice.get_camera_id("GelSight Mini")
         dev = gsdevice.Camera(finger, cam_id)
@@ -67,6 +52,8 @@ def main(argv):
 
     f0 = dev.get_raw_image()
     print('image size = ', f0.shape[1], f0.shape[0])
+    roi = (0, 0, f0.shape[1], f0.shape[0])
+
     if FIND_ROI:
         roi = cv2.selectROI(f0)
         roi_cropped = f0[int(roi[1]):int(roi[1] + roi[3]), int(roi[0]):int(roi[0] + roi[2])]
@@ -74,16 +61,14 @@ def main(argv):
         print('Press q in ROI image to continue')
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-    elif f0.shape == (640,480,3):
+    elif f0.shape == (640,480,3) and device != 'mini':
         roi = (60, 100, 375, 380)
-    elif f0.shape == (320,240,3):
+    elif f0.shape == (320,240,3) and device != 'mini':
         roi = (30, 50, 186, 190)
-    elif f0.shape == (240,320,3):
+    elif f0.shape == (240,320,3) and device != 'mini':
         ''' cropping is hard coded in resize_crop_mini() function in gsdevice.py file '''
         border_size = 0 # default values set for mini to get 3d
         roi = (border_size,border_size,320-2*border_size,240-2*border_size) # default values set for mini to get 3d
-    else:
-        roi = (0, 0, f0.shape[1], f0.shape[0])
 
     print('roi = ', roi)
     print('press q on image to exit')
