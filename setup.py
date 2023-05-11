2de2e8a5eacb6f7294f5e8b2ce1a8ce59616c5ec
GelSight <debra_shure@gelsight.com>
2023-05-11 15:56:08 -0400

    update dependencies and version

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='gelsight',
-    version='2.0',
-    packages=['gelsight'],
+    version='3.0',
+    python_requires='>=3.8',
+    packages=find_packages(),
     license='',
     author='gelsight',
     author_email='info@gelsight.com',
     install_requires=[
-        'numpy', 'numba', 'open3d', 'opencv_python', 'scipy', 'torch'
+        'cvbridge3>=1.1',
+        'numpy>=1.17.4',
+        'open3d>=0.12.0',
+        'opencv_python_headless>=4.7.0.68',
+        'Pillow>=9.5.0',
+        'pygrabber>=0.1',
+        'pyusb>=1.2.1',
+        'rospy>=1.15.14',
+        'scikit_image>=0.18.3',
+        'scipy>=1.10.1',
+        'sensor_msgs>=1.13.1',
+        'setuptools>=45.2.0',
+        'torch>=1.11.0'
     ],
-    description='A toolkit to interface with Gelsight tactile devices'
+    description='A toolkit to interface with GelSight tactile sensors'
 )
