fd95887f18043d9ac1efb58e08cb26c3678357f6
debrashure <debra_shure@gelsight.com>
2023-08-23 16:03:49 -0400

    update comments

--- gelsight/gsdevice.py
+++ gelsight/gsdevice.py
@@ -55,10 +55,12 @@ if os.name == 'nt':
 
 
 def resize_crop_mini(img, imgw, imgh):
-    # resize, crop and resize back
-    border_size_x, border_size_y = int(img.shape[0] * (1 / 7)), int(np.floor(img.shape[1] * (1 / 7)))  # remove 1/7th of border from each size
+    # remove 1/7th of border from each size
+    border_size_x, border_size_y = int(img.shape[0] * (1 / 7)), int(np.floor(img.shape[1] * (1 / 7)))
+    # keep the ratio the same as the original image size
     img = img[border_size_x+2:img.shape[0] - border_size_x, border_size_y:img.shape[1] - border_size_y]
-    img = cv2.resize(img, (imgw, imgh))  # final resize for 3d
+    # final resize for 3d
+    img = cv2.resize(img, (imgw, imgh))
     return img
 
 
